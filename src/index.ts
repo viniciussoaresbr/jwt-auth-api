7bb620692cc51789afc44f141fec9c7b1eab39f6
Vinicius Soares <viniciussoaresmbr@gmail.com>
2023-01-26 13:00:49 -0300

    configurando api para o heroku

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import cors from "cors";
 
 const app = express();
 
+const port = process.env.PORT || 3001;
+
 app.use(cors());
 app.use(express.json());
 
 app.use(router);
 
-app.listen(3001);
+app.listen(port);
